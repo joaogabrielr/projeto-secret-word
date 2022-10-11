2a707af6c86b2573dce00bef3d82a234f5f4fa1b
João Gabriel Ribeiro <joaogabrielrsousa@gmail.com>
2022-10-11 16:27:30 -0300

    Adicionei pontuação final à pagina gameover.js

--- src/App.js
+++ src/App.js
@@ -125,7 +125,7 @@ function App() {
       guesses={guesses}
       score={score}/>}
 
-      {gameStage === "end" && <GameOver retry={retry}/>}
+      {gameStage === "end" && <GameOver retry={retry} score={score}/>}
     </div>
   );
 }
