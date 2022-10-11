9fe68eb43f4748aae2bede621065cbec1030a196
João Gabriel Ribeiro <joaogabrielrsousa@gmail.com>
2022-10-11 15:54:38 -0300

    Criei states para as letras chutadas, erradas, score, tentativas

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("")
   const [letters, setLetters] = useState([])
 
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(0)
 
   //pick a random word from a random category
   const pickWordAndCategory = () => {
@@ -71,7 +75,18 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame}/>}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+
+      {gameStage === "game" && 
+      <Game 
+      verifyLetter={verifyLetter}
+      pickedWord={pickedWord}
+      pickedCategory={pickedCategory}
+      letters={letters}
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters}
+      guesses={guesses}
+      score={score}/>}
+
       {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   );
