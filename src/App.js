3dfafe65b6651eb0ab374ce6b10e599abb85bd07
João Gabriel Ribeiro <joaogabrielrsousa@gmail.com>
2022-10-11 14:34:33 -0300

    Criei todos os componentes do jogo, e importei o que será utilizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+//css
 import './App.css';
+
+//react
+import {useCallbak, useEffect, useState} from "react"
+
+//data
+import {wordsList} from "./data/words"
+
+//components
 import StartScreen from './components/StartScreen';
 
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+]
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen/>}
+      {gameStage === "game" && <StartScreen/>}
+      {gameStage === "end" && <StartScreen/>}
     </div>
   );
 }
